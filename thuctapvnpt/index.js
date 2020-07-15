9ef93bec9360ba13781a3ee303d4dd4c1260c692
dung0512 <58897384+dung0512@users.noreply.github.com>
2020-07-15 09:01:50 +0700

    web

--- thuctapvnpt/index.js
+++ thuctapvnpt/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    $(window).scroll(function(){
+        if(this.scrollY < 10)
+            $(".page-wrap").addclass(".sticky");
+        else{
+            $(".page-wrap").removeClass("sticky");
+        }
+    });
     $('.menu-toggler').click(function(){
         $(this).toggleClass("active");
         $(".first").toggleClass("active");
